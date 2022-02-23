c98e1114423b7da2d9260d1a160bd54d7c957655
Gennaro Raiola <gennaro.raiola@gmail.com>
2022-02-23 12:38:50 +0100

    LICENSE

--- include/wolf_hardware_interface/wolf_robot_hw.h
+++ include/wolf_hardware_interface/wolf_robot_hw.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2022 multidof
- * Author: Gennaro Raiola, Michele Focchi
+ * Copyright (C) 2022 Gennaro Raiola
+ * Author: Gennaro Raiola
  * email:  gennaro.raiola@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
